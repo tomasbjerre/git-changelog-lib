59830c6f2a394fe43801564939242ce8fc7071c3
Tomas Bjerre <tomas.bjerre85@gmail.com>
2015-11-15 10:24:47 +0100

    Customizing changelog #feature

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -1,5 +1,6 @@
 #/bin/bash
+ROOT_FOLDER=`pwd`
 cd build/distributions
 unzip *T.zip
 cd *T
-./bin/git-changelog-lib -std
+./bin/git-changelog-lib -std -t $ROOT_FOLDER/changelog.mustache -sf $ROOT_FOLDER/changelog.json
