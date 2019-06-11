6515299f938c982fd71d2d76b13eeccf6244b8bf
Tomas Bjerre <tomas.bjerre85@gmail.com>
2019-06-11 18:54:48 +0200

    Removing dependency on javax.xml

--- src/main/java/se/bjurr/gitchangelog/internal/integrations/rest/RestClient.java
+++ src/main/java/se/bjurr/gitchangelog/internal/integrations/rest/RestClient.java
@@ -4,7 +4,6 @@ import static com.google.common.base.Optional.absent;
 import static com.google.common.base.Optional.of;
 import static com.google.common.cache.CacheBuilder.newBuilder;
 import static com.google.common.io.ByteStreams.toByteArray;
-import static javax.xml.bind.DatatypeConverter.printBase64Binary;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import com.google.common.annotations.VisibleForTesting;
@@ -14,6 +13,7 @@ import com.google.common.cache.LoadingCache;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Base64;
 import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import se.bjurr.gitchangelog.api.exceptions.GitChangelogIntegrationException;
@@ -40,7 +40,7 @@ public class RestClient {
   public RestClient withBasicAuthCredentials(final String username, final String password) {
     try {
       this.basicAuthString =
-          new String(printBase64Binary((username + ":" + password).getBytes("UTF-8")));
+          Base64.getEncoder().encodeToString((username + ":" + password).getBytes("UTF-8"));
     } catch (final UnsupportedEncodingException e) {
       throw new RuntimeException(e);
     }
