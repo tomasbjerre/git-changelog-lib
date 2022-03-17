8c61d9ef127653cce42519c193afad063704e6dc
Tomas Bjerre <tomas.bjerre85@gmail.com>
2022-03-17 20:28:02 +0100

    chore(release): 1.165.2 [GRADLE SCRIPT]

--- src/main/java/se/bjurr/gitchangelog/api/GitChangelogApi.java
+++ src/main/java/se/bjurr/gitchangelog/api/GitChangelogApi.java
@@ -9,6 +9,11 @@ import static se.bjurr.gitchangelog.api.GitChangelogApiConstants.ZERO_COMMIT;
 import static se.bjurr.gitchangelog.internal.git.GitRepoDataHelper.removeCommitsWithoutIssue;
 import static se.bjurr.gitchangelog.internal.settings.Settings.fromFile;
 
+import com.github.jknack.handlebars.Context;
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.FileTemplateLoader;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
@@ -23,15 +28,7 @@ import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-
 import org.eclipse.jgit.lib.ObjectId;
-
-import com.github.jknack.handlebars.Context;
-import com.github.jknack.handlebars.Handlebars;
-import com.github.jknack.handlebars.Helper;
-import com.github.jknack.handlebars.Template;
-import com.github.jknack.handlebars.io.FileTemplateLoader;
-
 import se.bjurr.gitchangelog.api.exceptions.GitChangelogRepositoryException;
 import se.bjurr.gitchangelog.api.helpers.Helpers;
 import se.bjurr.gitchangelog.api.model.Changelog;
@@ -634,9 +631,9 @@ public class GitChangelogApi {
     } else {
       final Optional<ObjectId> headOpt = gitRepo.findRef(REF_HEAD);
       if (headOpt.isPresent()) {
-    	  toId = headOpt.get();
+        toId = headOpt.get();
       } else {
-    	  toId = gitRepo.getRef(REF_MASTER);
+        toId = gitRepo.getRef(REF_MASTER);
       }
     }
     GitRepoData gitRepoData =
