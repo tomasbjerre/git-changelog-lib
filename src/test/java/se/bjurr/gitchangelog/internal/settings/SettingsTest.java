74a6363734c252c439cf771f261ddcd921f7fd04
Réda Housni Alaoui <reda-alaoui@hey.com>
2023-07-24 16:44:28 +0200

    Add tests

---
+++ src/test/java/se/bjurr/gitchangelog/internal/settings/SettingsTest.java
@@ -0,0 +1,26 @@
+package se.bjurr.gitchangelog.internal.settings;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+/**
+ * @author Réda Housni Alaoui
+ */
+public class SettingsTest {
+
+	@Test
+	public void nullFromRevisionCanBeSet() {
+		Settings settings = new Settings();
+		settings.setFromRevision(null);
+		assertThat(settings.getFromRevision()).isEmpty();
+	}
+
+	@Test
+	public void nullToRevisionCanBeSet() {
+		Settings settings = new Settings();
+		settings.setToRevision(null);
+		assertThat(settings.getToRevision()).isEmpty();
+	}
+
+}
