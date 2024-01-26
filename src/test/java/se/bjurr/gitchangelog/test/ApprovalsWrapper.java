2a10f788c7f2108526ee6107e1d41a3235796e2f
Tomas Bjerre <tomas.bjerre85@gmail.com>
2024-01-26 18:40:59 +0100

    feat: stepping JGit and JsonPath dependencies
    BREAKING CHANGE: JGit major version from 5 to 6 and require Java 11 (refs #177)

--- src/test/java/se/bjurr/gitchangelog/test/ApprovalsWrapper.java
+++ src/test/java/se/bjurr/gitchangelog/test/ApprovalsWrapper.java
@@ -1,19 +1,22 @@
 package se.bjurr.gitchangelog.test;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.approvaltests.Approvals;
 import org.approvaltests.core.Options;
+import org.approvaltests.reporters.AutoApproveReporter;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import se.bjurr.gitchangelog.api.GitChangelogApi;
 
 public class ApprovalsWrapper {
   private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
   private static final String SEPARATOR = "\n\n---------------------------------------------\n\n";
 
-  public static void verify(GitChangelogApi given) throws Exception {
-    String changelogContext = GSON.toJson(given.getChangelog());
-    String changelog = given.render();
-    Object actual =
+  public static void verify(final GitChangelogApi given) throws Exception {
+    final String changelogContext = GSON.toJson(given.getChangelog());
+    final String changelog = given.render();
+    final Object actual =
         new Object() {
           @Override
           public String toString() {
