6f83c43c845d968732e74ad19e7a517d5544e66f
Tomas Bjerre <tomas.bjerre85@gmail.com>
2024-10-02 18:36:27 +0200

    fix: build script

--- release.sh
+++ release.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+set -x
+
+#
+# Getting classpath issues when using this lib together with git-changelog-gradle-plugin
+#
 
 nextVersion=$(npx git-changelog-command-line \
  --print-next-version) \
  && ./gradlew release -Pversion=$nextVersion \
+ && git commit -a -m "chore(release): ${nextVersion} [GRADLE SCRIPT]" \
+ && git push --follow-tags \
  && npx git-changelog-command-line \
   -of CHANGELOG.md \
-  --ignore-pattern "^\\[maven-release-plugin\\].*|^\\[Gradle Release Plugin\\].*|^Merge.*|.*\\[GRADLE SCRIPT\\].*"
+  --ignore-pattern "^\\[maven-release-plugin\\].*|^\\[Gradle Release Plugin\\].*|^Merge.*|.*\\[GRADLE SCRIPT\\].*" \
+ && git commit -a -m "chore(release): Updating changelog with ${nextVersion} [GRADLE SCRIPT]" \
+ && git push
