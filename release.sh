235b03ef3d7c7ff95d9c06802d767b12439aaa6d
Tomas Bjerre <tomas.bjerre85@gmail.com>
2024-10-02 19:06:21 +0200

    chore: build script

--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ nextVersion=$(npx git-changelog-command-line \
  --print-next-version) \
  && ./gradlew release -Dorg.gradle.project.version=$nextVersion \
  && git tag $nextVersion \
- && git push --follow-tags \
+ && git push --tags \
  && npx git-changelog-command-line \
   -of CHANGELOG.md \
   --ignore-pattern "^\\[maven-release-plugin\\].*|^\\[Gradle Release Plugin\\].*|^Merge.*|.*\\[GRADLE SCRIPT\\].*" \
